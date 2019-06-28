d0a1aac68da91399896cab75802ff0df211a654b
Mateus Vinícius <mateus.oliveira21@fatecitapetininga.edu.br>
2019-06-28 00:44:39 -0300

    Heroku database test. Again.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,7 +4,7 @@ const uristring =
     process.env.MONGODB_URI ||
     'mongodb://127.0.0.1:27017'
 
-mongoose.connect(`${uristring}/pwa-api`, {
+mongoose.connect(`${uristring}`, {
   useNewUrlParser: true,
   useCreateIndex: true
 })
